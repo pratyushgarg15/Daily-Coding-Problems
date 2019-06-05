6245e9616cc8d01dc1361bf750592f50e658ff11
pratyushgarg15 <43318389+pratyushgarg15@users.noreply.github.com>
2019-06-05 11:34:51 +0530

    Update day 4.py

--- day 4.py
+++ day 4.py
@@ -24,17 +24,22 @@ def lowest_integer2(arr):
     for i in range(len(arr)):
         if(arr[i] < minimum and arr[i] > 0):
             minimum = arr[i]
-            
+    
+    '''if min positive integer is not 1 return 1'''
     if (minimum != 1):
         return 1
     
+    '''create a hash table with the elements of the array as key'''
     for val in arr:
         hash_table[val] = 1
-     
+    
+    '''loop over numbers from min +ve integer to max +ve integer 
+       and return the first element that is not in the hash table'''
     for i in range(minimum+1,maximum):
         if i not in hash_table:
             return i
-            
+    
+    '''if numbers from min to max are contiguous return max+1'''
     return maximum+1
 
 def lowest_integer1(arr):
@@ -43,17 +48,21 @@ def lowest_integer1(arr):
     count = 0
     maximum = max(arr)
     
+    '''segregate and shift all negative integers to the left 
+       side of the array'''
     for i in range(len(arr)-1,-1,-1):
         if(arr[i]<=0):
             arr[i] , arr[j] = arr[j] , arr[i]
             j-=1
             count+=1
 
+    '''mark indices negative signifying that an element is 
+       present in the array'''        
     for i in range(0,len(arr)-count):
         if(abs(arr[i])-1 < len(arr) and arr[abs(arr[i])-1] > 0):
             arr[abs(arr[i])-1] = -arr[abs(arr[i])-1]
     
-
+    '''return the index + 1 of first element that is not negative'''
     for i in range(len(arr)):
         if(arr[i]>0):
             return i+1
